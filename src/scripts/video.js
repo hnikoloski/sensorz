d7f12b35eb0ec9dbab3d8352f323578d0eae8905
hnikoloski <h.nikoloski@outlook.com>
2023-02-26 16:17:32 +0100

    .

--- src/scripts/video.js
+++ src/scripts/video.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function ($) {
         loop: true,
         muted: true,
         fluid: true,
-
+        textTrackSettings: false,
         // poster is the poster attribute on the video tag
         poster: $("#hero-video .video-js").attr("poster"),
         // sources is the sources attribute on the video tag
@@ -25,13 +25,14 @@ jQuery(document).ready(function ($) {
 
     });
 
-    var player = videojs("video-infographic", {
+    var player2 = videojs("video-infographic", {
         controls: false,
         autoplay: true,
         preload: "auto",
         loop: true,
         muted: true,
         fluid: true,
+        textTrackSettings: false,
 
         // poster is the poster attribute on the video tag
         poster: $("#video-infographic .video-js").attr("poster"),
